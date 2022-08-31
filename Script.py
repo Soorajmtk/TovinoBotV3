cc36df8691c082a65534e1394aef2a9f99308615
Soorajmtk <108129312+Soorajmtk@users.noreply.github.com>
2022-08-31 08:55:20 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -25,7 +25,7 @@ class script(object):
 """
     ABOUT_TXT = """
 ○ 𝖬𝗒 𝖭𝖺𝗆e : ᴋᴜʀᴜᴘ ʙᴏᴛ
-○ 𝖢𝗋𝖾𝖺𝗍𝗈𝗋 : <a href=http://t.me/sujithkr320'>sᴜᴊɪᴛʜ ᴋ ʀ</a>
+○ 𝖢𝗋𝖾𝖺𝗍𝗈𝗋 : <ahref=http://t.me/AkkuMvk'>sᴜᴊɪᴛʜ ᴋ ʀ</a>
 ○ 𝖫𝖺𝗇𝗀𝗎𝖺𝗀𝖾 : 𝖯𝗒𝗍𝗁𝗈𝗇 𝟥 
 ○ 𝖫𝗂𝖻𝗋𝖺𝗋𝗒 : 𝖯𝗒𝗋𝗈𝗀𝗋𝖺𝗆 𝖺𝗌𝗒𝗇𝖼𝗂𝗈 𝟢.𝟣𝟩.𝟣 
 ○ 𝖲𝖾𝗋𝗏𝖾𝗋 : Contabo
